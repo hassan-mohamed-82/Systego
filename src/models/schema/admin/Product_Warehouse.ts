5ac87b38fc00dda312cbc8dbd0ce200d27547144
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-10-05 13:41:15 +0300

    update product_warehouse

--- src/models/schema/admin/Product_Warehouse.ts
+++ src/models/schema/admin/Product_Warehouse.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const Product_transferSchema = new mongoose.Schema({
+const Product_WarehouseSchema = new mongoose.Schema({
   productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
   WarehouseId: { type: mongoose.Schema.Types.ObjectId, ref: "Warehouse", required: true },
     quantity: { type: Number, required: true },
 });
-export const Product_transferModel = mongoose.model("Product_transfer", Product_transferSchema);
+export const Product_WarehouseModel = mongoose.model("Product_transfer", Product_WarehouseSchema);
--- src/models/schema/admin/product_transfer.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const Product_transferSchema = new mongoose.Schema({
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-  WarehouseId: { type: mongoose.Schema.Types.ObjectId, ref: "Warehouse", required: true },
-    quantity: { type: Number, required: true },
-});
-export const Product_transferModel = mongoose.model("Product_transfer", Product_transferSchema);
