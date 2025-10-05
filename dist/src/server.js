cc65d440e28d13592cc332cb2c2287af0faa9907
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-10-05 14:05:55 +0300

    test

--- dist/src/server.js
+++ dist/src/server.js
@@ -47,7 +47,7 @@ io.on("connection", (socket) => {
 });
 // 🕒 Start cron jobs (expiry & low stock check)
 (0, expiry_lowstock_1.startCron)(io);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
