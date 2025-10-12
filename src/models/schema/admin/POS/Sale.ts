d911e07f48dddea4e20e78776da0f67c6bc8c6a3
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-10-12 16:53:08 +0300

    feat Sale : add sale api

---
+++ src/models/schema/admin/POS/Sale.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const SaleSchema = new Schema(
+  {
+    customer_id: { type: Schema.Types.ObjectId, ref: 'Customer', required: true },
+    warehouse_id: { type: Schema.Types.ObjectId, ref: 'Warehouse', required: true },
+    currency_id: { type: Schema.Types.ObjectId, ref: 'Currency' },
+    sale_status: { type: String, required: true, default: 'pending', enum: ['completed', 'pending', 'returned', 'draft', 'processing'] },
+    order_tax: { type: Schema.Types.ObjectId, ref: 'Taxes' },
+    order_discount: { type: Schema.Types.ObjectId, ref: 'Discount' },
+    shipping_cost: { type: Number, default: 0 },
+    grand_total: { type: Number, required: true },
+    coupon_id: { type: Schema.Types.ObjectId, ref: 'Coupon' },
+    gift_card_id: { type: Schema.Types.ObjectId, ref: 'GiftCard' }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const productSalesSchema = new Schema({
+    sale_id: { type: Schema.Types.ObjectId, ref: 'Sale', required: true },
+    product_id: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, required: true, min: 0 },
+    price: { type: Number, required: true, min: 0 },
+    subtotal: { type: Number, required: true, min: 0 },
+    options_id: { type: Schema.Types.ObjectId, ref: 'ProductOption' }
+  },
+  { timestamps: true }
+);
+export const SaleModel = mongoose.model("Sale", SaleSchema);
+export const ProductSalesModel = mongoose.model("ProductSale", productSalesSchema);
