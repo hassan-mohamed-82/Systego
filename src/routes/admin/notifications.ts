152047325512ab1cc8f51ff5ed421077c8ad72f4
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-10-27 14:10:22 +0300

    get all notification unread

--- src/routes/admin/notifications.ts
+++ src/routes/admin/notifications.ts
@@ -5,6 +5,6 @@ import {getAllNotifications,getNotificationById ,getallnotficationsunread} from 
 const router = Router();
 
 router.get("/",catchAsync(getAllNotifications));
-router.get("/:id",catchAsync(getNotificationById));
 router.get("/unread",catchAsync(getallnotficationsunread));
+router.get("/:id",catchAsync(getNotificationById));
 export default router;
