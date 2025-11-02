4db6859c80999ff695038036f874e592b6e9b37d
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-11-02 11:21:12 +0200

    update country

--- dist/src/validation/admin/country.js
+++ dist/src/validation/admin/country.js
@@ -8,8 +8,10 @@ const joi_1 = __importDefault(require("joi"));
 exports.createcountrySchema = joi_1.default.object({
     name: joi_1.default.string().max(100).required(),
     isDefault: joi_1.default.boolean().optional(),
+    ar_name: joi_1.default.string().max(100).required(),
 });
 exports.updatecountrySchema = joi_1.default.object({
     name: joi_1.default.string().max(100).optional(),
     isDefault: joi_1.default.boolean().optional(),
+    ar_name: joi_1.default.string().max(100).optional(),
 });
--- src/validation/admin/country.ts
+++ src/validation/admin/country.ts
@@ -3,9 +3,11 @@ import Joi from "joi";
 export const createcountrySchema = Joi.object({
   name: Joi.string().max(100).required(),
   isDefault: Joi.boolean().optional(),
+  ar_name: Joi.string().max(100).required(),
 });
 
 export const updatecountrySchema = Joi.object({
   name: Joi.string().max(100).optional(),
   isDefault: Joi.boolean().optional(),
+  ar_name: Joi.string().max(100).optional(),
 });
