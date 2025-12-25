804585d13ce4e721a38dc289301348d73ce0dfe9
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-12-25 13:25:09 +0200

    update customerRouter

--- src/routes/admin/customerRoutes.ts
+++ src/routes/admin/customerRoutes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import {authorizePermissions} from "../../middlewares/haspremission"
-import { createCustomer, deleteCustomer, getCustomerById, getCustomers, getDueCustomers, updateCustomer } from '../../controller/admin/customer';
+import { createCustomer, deleteCustomer, getCustomerById, getCustomers, getDueCustomers, updateCustomer 
+    ,getallgroups,getgroupbyid
+} from '../../controller/admin/customer';
 import { catchAsync } from '../../utils/catchAsync';
 
 const router = express.Router();
 
 router.post('/',authorizePermissions("POS","Add"),authorizePermissions("customer","Add"), catchAsync(createCustomer));
 router.get('/',authorizePermissions("customer","View"), catchAsync(getCustomers));
+router.get('/groups',authorizePermissions("customer_group","View"), catchAsync(getallgroups));
+router.get('/groups/:id',authorizePermissions("customer_group","View"), catchAsync(getgroupbyid));
 router.get('/:id',authorizePermissions("customer","View"), catchAsync(getCustomerById));
 router.get('/due',authorizePermissions("customer","View"), catchAsync(getDueCustomers));
 router.put('/:id',authorizePermissions("customer","Edit"), catchAsync(updateCustomer));
