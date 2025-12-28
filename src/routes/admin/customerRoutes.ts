5e7100a4ccb0e772cdf63cfb564028b0fc1eab7d
Miskovy <mazenkhairy200@gmail.com>
2025-12-28 10:13:47 +0200

    fix routes for customer group

--- src/routes/admin/customerRoutes.ts
+++ src/routes/admin/customerRoutes.ts
@@ -10,15 +10,19 @@ const router = express.Router();
 
 router.post('/', authorizePermissions("POS", "Add"), authorizePermissions("customer", "Add"), catchAsync(createCustomer));
 router.get('/', authorizePermissions("customer", "View"), catchAsync(getCustomers));
-router.get('/:id', authorizePermissions("customer", "View"), catchAsync(getCustomerById));
-router.get('/due', authorizePermissions("customer", "View"), catchAsync(getDueCustomers));
-router.put('/:id', authorizePermissions("customer", "Edit"), catchAsync(updateCustomer));
-router.delete('/:id', authorizePermissions("customer", "Delete"), catchAsync(deleteCustomer));
 
-// Group Routes
+// Due customers route (must be before /:id)
+router.get('/due', authorizePermissions("customer", "View"), catchAsync(getDueCustomers));
+
+// Group Routes (must be before /:id to prevent "group" being treated as customer ID)
 router.post('/group', authorizePermissions("customer_group", "Add"), catchAsync(creategroup));
 router.get('/group', authorizePermissions("customer_group", "View"), catchAsync(getallgroups));
 router.get('/group/:id', authorizePermissions("customer_group", "View"), catchAsync(getgroupbyid));
 router.put('/group/:id', authorizePermissions("customer_group", "Edit"), catchAsync(updategroup));
 router.delete('/group/:id', authorizePermissions("customer_group", "Delete"), catchAsync(deletegroup));
+
+// Generic customer routes with :id parameter (must be LAST)
+router.get('/:id', authorizePermissions("customer", "View"), catchAsync(getCustomerById));
+router.put('/:id', authorizePermissions("customer", "Edit"), catchAsync(updateCustomer));
+router.delete('/:id', authorizePermissions("customer", "Delete"), catchAsync(deleteCustomer));
 export default router;
