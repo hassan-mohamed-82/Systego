5e9ab3b2ef6debf784585c1a2754ab1f817b6335
Miskovy <mazenkhairy200@gmail.com>
2025-12-29 15:35:06 +0200

    fix sale routes dist

--- dist/src/routes/admin/POS/POSRoutes.js
+++ dist/src/routes/admin/POS/POSRoutes.js
@@ -12,8 +12,9 @@ router.post('/sales', (0, haspremission_1.authorizePermissions)("POS", "Add"), (
 router.get('/sales', (0, haspremission_1.authorizePermissions)("POS", "View"), (0, catchAsync_1.catchAsync)(saleController_1.getAllSales));
 router.get('/sales/pending', (0, haspremission_1.authorizePermissions)("POS", "View"), (0, catchAsync_1.catchAsync)(saleController_1.getsalePending));
 router.post('/sales/complete', (0, haspremission_1.authorizePermissions)("POS", "View"), (0, catchAsync_1.catchAsync)(saleController_1.getShiftCompletedSales));
-router.get("/sales/:id", (0, haspremission_1.authorizePermissions)("POS", "View"), (0, catchAsync_1.catchAsync)(saleController_1.getSales));
-router.get("/sales/pending/:sale_id", (0, haspremission_1.authorizePermissions)("POS", "View"), (0, catchAsync_1.catchAsync)(saleController_1.getSalePendingById));
+// ⚠️ Static routes MUST come before dynamic :id routes
 router.get("/sales/dues", (0, haspremission_1.authorizePermissions)("POS", "View"), (0, catchAsync_1.catchAsync)(saleController_1.getDueSales));
 router.post("/sales/pay-due", (0, haspremission_1.authorizePermissions)("POS", "Add"), (0, catchAsync_1.catchAsync)(saleController_1.payDue));
+router.get("/sales/pending/:sale_id", (0, haspremission_1.authorizePermissions)("POS", "View"), (0, catchAsync_1.catchAsync)(saleController_1.getSalePendingById));
+router.get("/sales/:id", (0, haspremission_1.authorizePermissions)("POS", "View"), (0, catchAsync_1.catchAsync)(saleController_1.getSales));
 exports.default = router;
