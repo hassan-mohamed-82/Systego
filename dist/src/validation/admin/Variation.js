2176e8614f395f3fd123c3f553b6dd6fc3e45e4b
hs301443 <106682004+hs301443@users.noreply.github.com>
2026-02-17 13:23:24 +0200

    feat: Initialize server infrastructure and implement admin purchase management with new models, controllers, routes, and variation validation.

--- dist/src/validation/admin/Variation.js
+++ dist/src/validation/admin/Variation.js
@@ -6,8 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateVariationSchema = exports.createVariationSchema = void 0;
 const joi_1 = __importDefault(require("joi"));
 exports.createVariationSchema = joi_1.default.object({
-    name: joi_1.default.string().trim().min(1).max(200).required(),
-    ar_name: joi_1.default.string().trim().min(1).max(200).required(),
+    name: joi_1.default.string().trim().min(1).max(200).required().messages({
+        "string.empty": "Name is required",
+        "string.min": "Name must be at least 1 character long",
+        "string.max": "Name must be at most 200 characters long",
+        "any.required": "Name is required",
+        "any.unique": "Name already exists",
+    }),
+    ar_name: joi_1.default.string().trim().min(1).max(200).required().messages({
+        "string.empty": "Name is required",
+        "string.min": "Name must be at least 1 character long",
+        "string.max": "Name must be at most 200 characters long",
+        "any.required": "Name is required",
+        "any.unique": "Name already exists",
+    }),
     options: joi_1.default.array().items(joi_1.default.alternatives().try(joi_1.default.string().trim().min(1).max(200), joi_1.default.object({
         name: joi_1.default.string().trim().min(1).max(200).required(),
         status: joi_1.default.boolean().optional(),
--- src/validation/admin/Variation.ts
+++ src/validation/admin/Variation.ts
@@ -1,8 +1,20 @@
 import Joi from "joi";
 
 export const createVariationSchema = Joi.object({
-  name: Joi.string().trim().min(1).max(200).required(),
-  ar_name: Joi.string().trim().min(1).max(200).required(),
+  name: Joi.string().trim().min(1).max(200).required().messages({
+    "string.empty": "Name is required",
+    "string.min": "Name must be at least 1 character long",
+    "string.max": "Name must be at most 200 characters long",
+    "any.required": "Name is required",
+    "any.unique": "Name already exists",
+  }),
+  ar_name: Joi.string().trim().min(1).max(200).required().messages({
+    "string.empty": "Name is required",
+    "string.min": "Name must be at least 1 character long",
+    "string.max": "Name must be at most 200 characters long",
+    "any.required": "Name is required",
+    "any.unique": "Name already exists",
+  }),
   options: Joi.array().items(
     Joi.alternatives().try(
       Joi.string().trim().min(1).max(200),     
