e66daae541e9be23e0ad108c35d54fe7a4d05aa3
hs301443 <106682004+hs301443@users.noreply.github.com>
2026-02-23 14:23:41 +0200

    feat: Add product report routes for sales and selection.

--- src/routes/admin/productReport.ts
+++ src/routes/admin/productReport.ts
@@ -3,7 +3,6 @@ import { authorizePermissions } from "../../middlewares/haspremission";
 import { catchAsync } from "../../utils/catchAsync";
 import { getProductSalesReport, selection } from "../../controller/admin/productReport";
 const router = Router();
-
 router.post("/", authorizePermissions("product", "View"), catchAsync(getProductSalesReport));
 router.get("/selection", authorizePermissions("product", "View"), catchAsync(selection));
 
