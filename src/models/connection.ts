5adc18a8516db2e4b521d41b5c6757dbcf3b6b69
&mazen <mazenkhairy200@gmail.com>
2026-03-01 11:07:02 +0200

    debugging process kill

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -7,12 +7,12 @@ import dotenv from "dotenv";
 dotenv.config();
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MongoDB_URI||"",)
+    await mongoose.connect(process.env.MongoDB_URI || "",)
     console.log("MongoDB connected successfully");
 
   } catch (error) {
     console.error("MongoDB connection failed:", error);
-    process.exit(1); // Exit the process with failure
+    // process.exit(1); // Exit the process with failure
   }
 };
 
