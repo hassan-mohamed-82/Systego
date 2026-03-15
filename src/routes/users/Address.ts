dbb3688da8a218f4d3cdcf1b89751be2cbfcaba2
basantsaed2 <basantsaed718@gmail.com>
2026-03-15 09:46:44 +0200

    edit address

--- src/routes/users/Address.ts
+++ src/routes/users/Address.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { authenticated } from '../../middlewares/authenticated';
-import { addAddress, getMyAddresses, deleteAddress, updateAddress } from '../../controller/users/address';
+import { addAddress, getMyAddresses, deleteAddress, updateAddress, getAllLists } from '../../controller/users/address';
 import { validate } from '../../middlewares/validation';
 import { addressSchema, updateAddressSchema } from '../../validation/users/address';
 
 const addressRoute = Router();
 
 addressRoute.use(authenticated);
+
+addressRoute.get("/lists", getAllLists)
 
 addressRoute.post("/", validate(addressSchema), addAddress);
 addressRoute.put("/:id", validate(updateAddressSchema), updateAddress);
