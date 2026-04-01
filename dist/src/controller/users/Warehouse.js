4171309bfd0b5f14c7e7d63ac636ee9c5c648817
hs301443 <106682004+hs301443@users.noreply.github.com>
2026-04-01 09:43:01 +0200

    feat: add getWarehouses controller to fetch all warehouse records

--- dist/src/controller/users/Warehouse.js
+++ dist/src/controller/users/Warehouse.js
@@ -4,7 +4,7 @@ exports.getWarehouses = void 0;
 const Warehouse_1 = require("../../models/schema/admin/Warehouse");
 const response_1 = require("../../utils/response");
 const getWarehouses = async (req, res) => {
-    const warehouses = await Warehouse_1.WarehouseModel.find({ Is_Online: true });
+    const warehouses = await Warehouse_1.WarehouseModel.find();
     (0, response_1.SuccessResponse)(res, { message: "Get warehouses successfully", data: warehouses });
 };
 exports.getWarehouses = getWarehouses;
